cd68bae0082fa995f89034fdbc8a2d78f5627d76
Chelsea Li <chelseali2001@gmail.com>
2020-02-26 23:28:00 -0800

    Create BinaryTreeList.hs

---
+++ Trees/BinaryTreeList.hs
@@ -0,0 +1,13 @@
+module BinaryTreeList where
+
+data List a = Empty 
+            | Cons a (List a) 
+            deriving (Show, Read, Eq, Ord)  
+
+type ListZipper a = ([a],[a])  
+
+goForward :: ListZipper a -> ListZipper a  
+goForward (x:xs, bs) = (xs, x:bs)  
+  
+goBack :: ListZipper a -> ListZipper a  
+goBack (xs, b:bs) = (b:xs, bs)  
